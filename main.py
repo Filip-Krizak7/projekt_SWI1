5ebb23f709f4fd06b67820f34d98de5b322561b4
Filip-Krizak <r20382@student.osu.cz>
2022-02-17 23:00:05 +0100

    test vlozeni objednavky do databaze

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from apify_client import ApifyClient
+import pymysql
 
-# Initialize the ApifyClient with your API token
+connection = pymysql.connect(host="localhost",user="root",passwd="",database="test")
+cursor = connection.cursor()
+
 client = ApifyClient("apify_api_kY3re4QxTgH4Apsz0xyJVtrYiYmlEB0ahVcm")
 
 # Prepare the actor input
 run_input = {
-    "search": "New York",
+    "search": "Prague",
+    "destType": "city",
     "maxPages": 2,
     "sortBy": "distance_from_search",
     "currency": "USD",
@@ -15,9 +19,13 @@ run_input = {
     "extendOutputFunction": "($) => { return {} }",
 }
 
-# Run the actor and wait for it to finish
 run = client.actor("dtrungtin/booking-scraper").call(run_input=run_input)
 
-# Fetch and print actor results from the run's dataset (if there are any)
 for item in client.dataset(run["defaultDatasetId"]).iterate_items():
     print(item)
+
+insert_reservation = "INSERT INTO reservations(customer_ID, check_in_date, check_out_date, hotel, price) VALUES('1', '2022-04-06', '2022-04-09', 'Park In', '139');"
+cursor.execute(insert_reservation)
+
+connection.commit()
+connection.close()
